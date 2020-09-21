c0f121bee5e2c123e6f292c6a1fd1dcad6cf566a
jdamilaspmys <jdamilaspmys@gmail.com>
2020-09-21 12:41:29 +0800

    add logs

--- handler.js
+++ handler.js
@@ -4,6 +4,8 @@ const log = console
 
 module.exports.listEnv = async event => {
 
+    // SNS
+    // TOPIC 
     // SNS_EMAIL_FILTER_TOPIC: !Ref EmailFilterTopic
     // SNS_TECH_EMAIL_TOPIC: !Ref TechEmailTopic
     // SNS_OPS_EMAIL_TOPIC: !Ref OpsEmailTopic
@@ -12,18 +14,44 @@ module.exports.listEnv = async event => {
     const SNS_TECH_EMAIL_TOPIC = process.env.SNS_TECH_EMAIL_TOPIC
     const SNS_OPS_EMAIL_TOPIC = process.env.SNS_OPS_EMAIL_TOPIC
     const SNS_SALES_EMAIL_TOPIC = process.env.SNS_SALES_EMAIL_TOPIC
-    const env = { 
+    const topic = { 
         SNS_EMAIL_FILTER_TOPIC: SNS_EMAIL_FILTER_TOPIC, 
         SNS_TECH_EMAIL_TOPIC:   SNS_TECH_EMAIL_TOPIC,
         SNS_OPS_EMAIL_TOPIC:    SNS_OPS_EMAIL_TOPIC,
         SNS_SALES_EMAIL_TOPIC:  SNS_SALES_EMAIL_TOPIC
     }
+    const sns = { topic }
+    // SQS
+    // TECH_EMAIL_QUEUE: !Ref TechEmailQueue  
+    // OPS_EMAIL_QUEUE: !Ref OpsEmailQueue
+    // SALES_EMAIL_QUEUE: !Ref SalesEmailQueue
+    // DEAD_LETTER_TECH_EMAIL_QUEUE: !Ref DeadLetterTechEmailQueue  
+    // DEAD_LETTER_OPS_EMAIL_QUEUE: !Ref DeadLetterOpsEmailQueue
+    // DEAD_LETTER_SALES_EMAIL_QUEUE: !Ref DeadLetterSalesEmailQueue
+    const TECH_EMAIL_QUEUE = process.env.TECH_EMAIL_QUEUE
+    const OPS_EMAIL_QUEUE = process.env.OPS_EMAIL_QUEUE
+    const SALES_EMAIL_QUEUE = process.env.SALES_EMAIL_QUEUE
+    const DEAD_LETTER_TECH_EMAIL_QUEUE = process.env.DEAD_LETTER_TECH_EMAIL_QUEUE
+    const DEAD_LETTER_OPS_EMAIL_QUEUE = process.env.DEAD_LETTER_OPS_EMAIL_QUEUE
+    const DEAD_LETTER_SALES_EMAIL_QUEUE = process.env.DEAD_LETTER_SALES_EMAIL_QUEUE
+    const sqs = { 
+        TECH_EMAIL_QUEUE: TECH_EMAIL_QUEUE,
+        OPS_EMAIL_QUEUE: OPS_EMAIL_QUEUE,
+        SALES_EMAIL_QUEUE: SALES_EMAIL_QUEUE,
+        DEAD_LETTER_TECH_EMAIL_QUEUE: DEAD_LETTER_TECH_EMAIL_QUEUE,
+        DEAD_LETTER_OPS_EMAIL_QUEUE: DEAD_LETTER_OPS_EMAIL_QUEUE,
+        DEAD_LETTER_SALES_EMAIL_QUEUE: DEAD_LETTER_SALES_EMAIL_QUEUE
+    }
+
+    // Env object 
+    const env = { sns, sqs }    
+    // data object 
     const data = { env }
+    // response 
     const response = {
         statusCode: 200,
         body: JSON.stringify(data),
-    };
-    log.log(env)
+    };    
     return response;
 
 };
