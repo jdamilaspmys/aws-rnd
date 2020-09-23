3d74260e8f34b133e68e103f144c371def4a157c
jdamilaspmys <jdamilaspmys@gmail.com>
2020-09-23 14:44:45 +0800

    sendToSNSTopic mapper move as helper

--- src/handlers/processTechOpsSalesEmailSQS/processTechOpsSalesEmailSQS.js
+++ src/handlers/processTechOpsSalesEmailSQS/processTechOpsSalesEmailSQS.js
@@ -1,10 +1,5 @@
 const sns = require('../../models/sns')
-
-const sendToSNSTopicMapper = {
-  TECH : process.env.SNS_TECH_EMAIL_TOPIC,
-  OPS : process.env.SNS_OPS_EMAIL_TOPIC,
-  SALES : process.env.SNS_SALES_EMAIL_TOPIC,
-}
+const sendToSNSTopicMapper = require('../../helpers/sendToSNSTopicMapper')
 
 const processTechOpsSalesEmailSQS = async (data) => {
   try {     
---
+++ src/helpers/sendToSNSTopicMapper.js
@@ -0,0 +1,5 @@
+module.exports = {
+    TECH : process.env.SNS_TECH_EMAIL_TOPIC,
+    OPS : process.env.SNS_OPS_EMAIL_TOPIC,
+    SALES : process.env.SNS_SALES_EMAIL_TOPIC
+}
